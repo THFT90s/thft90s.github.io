f7edbc75e36da0ca6f0e1a0811b2eb9d20555616
Kwanza Olowokere <theholidayfromthe90s@gmail.com>
2026-02-04 23:52:25 -0600

    Added welcome div

--- site.js
+++ site.js
@@ -4,6 +4,24 @@ document.body.style.backgroundColor = 'oldface'
 const javascript = document.querySelector('h1')
 javascript.style.color = 'steelblue'
 
+//W3 Assignment
+const hours = new Date().getHours() // current hour
 
 
+const isMorning = hours >= 4 && hours < 12 //  morning
+const isAfterNoon = hours >= 12 && hours < 17 // afternoon
+const isEvening = hours >= 17 || hours < 4 // evening 
+
+let greeting
+
+if (isMorning){
+     greeting = "Good Morning Sunshine!"
+}else if (isAfterNoon){
+     greeting = "Good Afternoon! Where's my diet Dr.Kelp?"
+}else if(isEvening){
+     greeting = "Good Evening, Second wind?"
+}
+
+ div = document.querySelector('#welcome')
+ div.innerHTML = greeting
 
